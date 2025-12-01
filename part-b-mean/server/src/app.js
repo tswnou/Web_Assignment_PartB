a791f878c08f49dfe38a80505e0ff3c059756281
itsonou <itsonou@netcompany.com>
2025-12-01 14:25:05 +0200

    test2

--- part-b-mean/server/src/app.js
+++ part-b-mean/server/src/app.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-export default app;
-
+// --- TEST ROUTE ---
 app.get("/", (req, res) => {
   res.json({ message: "Server is running!" });
 });
+
+export default app;
