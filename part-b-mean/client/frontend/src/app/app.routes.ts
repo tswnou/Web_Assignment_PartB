de4ea723b03486703ae0ec0b94c7634da16d8659
itsonou <itsonou@netcompany.com>
2025-12-01 15:10:30 +0200

    test4

--- part-b-mean/client/frontend/src/app/app.routes.ts
+++ part-b-mean/client/frontend/src/app/app.routes.ts
@@ -13,4 +13,5 @@ export const routes: Routes = [
   { path: 'books', component: BooksComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
